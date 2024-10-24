c93b5e620acbf28581bc12d8b85d2cc6394fab42
VhalennnG <fanydanny237@gmail.com>
2024-10-25 07:56:22 +0800

    deploy commit (4)

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ app.use(cors());
 app.set("views", path.join(__dirname, "views"));
 
 // Start server in local
-// app.listen(3000, () => {
-//   console.log("Server is running on port 4000");
-// });
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
 
 // routes
 app.get("*", checkUser);
